2eb0d98ddcfae136a53920f9964e000020fecbea
Sam <super8989@gmail.com>
2020-02-06 21:28:38 -0500

    set up express server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
