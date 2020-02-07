1605f9d5ba5c8efe66d19b7f8c3bfdf2d5ffa782
Sam <super8989@gmail.com>
2020-02-06 23:11:49 -0500

    set up basic index.html

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
