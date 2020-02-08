7fefe99dac2bb73c4111d5b37db85bf8054f0b97
Sam <super8989@gmail.com>
2020-02-07 20:38:51 -0500

    added api call

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/index.html`);
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+	// console.log(req.body.crypto);
+
+	request('https://blockchain.info/ticker', (error, response, body) => {
+		const data = JSON.parse(body);
+		const price = data.USD.last;
+
+		console.log(price);
+	});
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
