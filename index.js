789c03fce2543f870b0ffe322293a3be69b907f5
Sam <super8989@gmail.com>
2020-02-07 21:58:27 -0500

    receive data from api

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-	// console.log(req.body.crypto);
+	// console.log(req.body);
+	const crypto = req.body.crypto;
+	const fiat = req.body.fiat;
 
-	request('https://blockchain.info/ticker', (error, response, body) => {
-		const data = JSON.parse(body);
-		const price = data.USD.last;
+	request(
+		`https://api.nomics.com/v1/currencies/ticker?key=demo-26240835858194712a4f8cc0dc635c7a&ids=${crypto}&convert=${fiat}`,
+		(error, response, body) => {
+			const data = JSON.parse(body);
+			// console.log(data[0].price);
+			const price = data[0].price;
+			const dollar = Number(price).toFixed(2);
 
-		console.log(price);
-	});
+			res.send(`<h1>The price of ${crypto} is ${dollar} ${fiat} </h1>`);
+		}
+	);
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
